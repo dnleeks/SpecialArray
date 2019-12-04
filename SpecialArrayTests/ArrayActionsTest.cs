677bf61aba5e99170fc456e2863fdeb7bf12f058
dnleeks <dnleeks@infoconduits.com>
2019-12-04 13:32:27 -0800

    SortArrayWithManyElementsShouldSortTheArray passes

--- SpecialArrayTests/ArrayActionsTest.cs
+++ SpecialArrayTests/ArrayActionsTest.cs
@@ -30,7 +30,23 @@ namespace SpecialArrayTests
             // Arrange
             var items = new List<int>(new[] { 2, 3, 1 });
             SpecialArray.Collections.SpecialArray array = SpecialArray.Collections.SpecialArray.From(items);
-            var expectedItems = new List<int>(new[] { 1,2,3 });
+            var expectedItems = new List<int>(new[] { 1, 2, 3 });
+
+            // Act
+            ArrayActions.Sort(array);
+
+            // Assert 
+            array.Items.Should().BeEquivalentTo(expectedItems);
+
+        }
+
+        [TestMethod]
+        public void SortArrayWithManyElementsShouldSortTheArray()
+        {
+            // Arrange
+            var items = new List<int>(new[] { 7, 8, -2, 6, 2, 3, 1 });
+            SpecialArray.Collections.SpecialArray array = SpecialArray.Collections.SpecialArray.From(items);
+            var expectedItems = new List<int>(new[] { -2, 1, 2, 3, 6, 7, 8 });
 
             // Act
             ArrayActions.Sort(array);
