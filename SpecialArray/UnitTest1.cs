e7864594e239bc1ec42de0d7cb4fb0d9ca35a175
dnleeks <dnleeks@infoconduits.com>
2019-12-04 12:33:08 -0800

    Add project files.

---
+++ SpecialArray/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecialArray
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
