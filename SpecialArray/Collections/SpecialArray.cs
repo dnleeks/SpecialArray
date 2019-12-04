3352a1e87175f25cd3fa012f4b067df86445b9cf
dnleeks <dnleeks@infoconduits.com>
2019-12-04 13:13:33 -0800

    SpecialArrayWithValidIndexSpecifiedShouldReturnElement passes

---
+++ SpecialArray/Collections/SpecialArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace SpecialArray.Collections
+{
+    public class SpecialArray
+    {
+        private List<int> items;
+
+        private SpecialArray(List<int> input)
+        {
+            items = new List<int>(input);
+        }
+
+        public static SpecialArray From(List<int> input)
+        {
+            return new SpecialArray(input);
+        }
+
+        public int this[int index]
+        {
+            get => items[index];
+        }
+
+        public int Size()
+        {
+            return items.Count;
+        }
+
+        public void Swap(int index)
+        {
+            int swap = items[0];
+
+            items[0] = items[index];
+            items[index] = swap;
+        }
+    }
+}
--- SpecialArrayTests/SpecialArrayTest.cs
+++ SpecialArrayTests/SpecialArrayTest.cs
@@ -1,18 +1,21 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
+using SpecialArray.Collections;
 
 namespace SpecialArrayTests
 {
     [TestClass]
-    public class SpecialArray
+    public class SpecialArrayTest
     {
         [TestMethod]
         public void SpecialArrayWithValidIndexSpecifiedShouldReturnElement()
         {
             // Arrange
-            SpecialArray array = SpecialArray.From(new[]{1,3,5,7});
+            var items = new List<int>(new []{ 1, 3, 5, 7 });
+            SpecialArray.Collections.SpecialArray array = SpecialArray.Collections.SpecialArray.From(items);
             const int index = 3;
-            const int expected = 5;
+            const int expected = 7;
 
             // Act
             int actual = array[index];
